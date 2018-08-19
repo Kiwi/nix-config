86cc4e7b5c3b9b01a420debff47944588b4e57c1
Adam Schaefers <sch@efers.org>
2018-08-19 14:38:17 -0700

    move desktop to external module

--- configuration.nix
+++ configuration.nix
@@ -1,156 +1,145 @@
 { config, pkgs, ... }:
+ {
+   imports = [
+     ./hardware-configuration.nix
+     ./rescue_boot.nix
+     ./desktop.nix
+   ];
 
-let myemacs = (pkgs.emacs.override {withGTK3=false; withGTK2=false; withX=true;}); in {
-  imports = [
-    ./hardware-configuration.nix
-    ./rescue_boot.nix
-  ];
+   boot.cleanTmpDir = true;
+   boot.loader.grub.enable = true;
+   boot.loader.grub.version = 2;
+   boot.loader.grub.devices = [
+     "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026B76820C5544"
+     "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026B76822C9FD0"
+   ];
+   boot.loader.grub.copyKernels = true;
+   boot.supportedFilesystems = [ "zfs" ];
+   services.zfs.autoScrub.enable = true;
+   services.zfs.autoSnapshot = {
+     enable = true;
+     frequent = 8;
+     hourly = 0;
+     daily = 21;
+     weekly = 0;
+     monthly = 0;
+   };
+   boot.zfs.forceImportAll = false;
+   boot.zfs.forceImportRoot = false;
+   boot.kernelParams = [ "elevator=noop intel_iommu=on iommu=pt boot.shell_on_fail" ];
+   boot.kernelModules = [ "coretemp" "kvm-intel" ];
+   networking.hostId = "007f0100";
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.devices = [
-    "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026B76820C5544"
-    "/dev/disk/by-id/ata-KINGSTON_SA400S37120G_50026B76822C9FD0"
-  ];
-  boot.loader.grub.copyKernels = true;
-  boot.supportedFilesystems = [ "zfs" ];
-  services.zfs.autoScrub.enable = true;
-  services.zfs.autoSnapshot = {
-    enable = true;
-    frequent = 8;
-    hourly = 0;
-    daily = 21;
-    weekly = 0;
-    monthly = 0;
-  };
-  boot.zfs.forceImportAll = false;
-  boot.zfs.forceImportRoot = false;
-  boot.kernelParams = [ "elevator=noop intel_iommu=on iommu=pt boot.shell_on_fail" ];
-  networking.hostId = "007f0100";
+   networking.hostName = "nixos";
+   networking.networkmanager.enable = true;
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+   i18n = {
+     consoleFont = "Lat2-Terminus16";
+     consoleKeyMap = "us";
+     defaultLocale = "en_US.UTF-8";
+   };
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+   time.timeZone = "America/Los_Angeles";
 
-  time.timeZone = "America/Los_Angeles";
+   environment.systemPackages = with pkgs; [
+     man man-pages posix_man_pages stdman
 
-  environment.sessionVariables = {
-    EDITOR = "emacsclient";
-    VISUAL = "emacsclient";
-  };
+     gptfdisk pmutils psmisc which file binutils bc utillinuxCurses exfat
+     dosfstools patchutils moreutils unzip zip
 
-  environment.shells = [
-    "${pkgs.bash}/bin/bash"
-  ];
+     lm_sensors htop iotop powertop
 
-  environment.systemPackages = with pkgs; [
-    gptfdisk
-    wget
-    curl
-    gnutls
-    gnupg1compat
-    pinentry
-    git
-    firefox
-    thunderbird
-    openvpn
-    qbittorrent
-    tmux
-    mpv
-    wmctrl
-    xorg.xmodmap
-    xorg.xset
-    xorg.xsetroot
-    numlockx
-    scrot
-    libnotify
-    pkgs.acpilight
-  (python36.withPackages(ps: with ps; [ certifi ]))
-    pandoc
-  ];
+     ltrace strace linuxPackages.perf
 
-  nixpkgs.config.packageOverrides = super: {
-    acpilight = pkgs.callPackage ./pkgs/acpilight.nix {};
-  };
+     smartmontools pciutils lshw usbutils
 
-  programs.bash.enableCompletion = true;
-  programs.mtr.enable = true;
-  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+     tmux wget curl inetutils nix-prefetch-scripts
 
-  services.openssh.enable = true;
+     (python36.withPackages(ps: with ps; [ certifi ]))
 
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  networking.firewall.allowedUDPPorts = [ 22 ];
+     gnutls gnupg gnupg1compat pinentry
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+     gitAndTools.gitFull gitAndTools.gitflow
+     pandoc
 
-  hardware.opengl = {
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+     chromium firefox thunderbird qbittorrent
+     openvpn
+     virtmanager
 
-  services.compton.enable = true;
-  services.compton.backend = "glx";
+     mpv
+     wmctrl scrot
+     pkgs.acpilight
+     xorg.xmodmap xorg.xset xorg.xsetroot xclip xsel numlockx 
+     libnotify 
+   ];
 
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    libinput.enable = true;
-    libinput.accelSpeed = "0.9";
-    videoDrivers = [ "modesetting" ];
-    useGlamor = true;
-    deviceSection = ''
-      Option "DRI" "3"
-      Option "TearFree" "true"
-      Option "AccelMethod" "glamor"
-    '';
+   nixpkgs.config.packageOverrides = super: {
+     acpilight = pkgs.callPackage ./pkgs/acpilight.nix {};
+   };
 
-    displayManager.slim.enable = true;
-    displayManager.slim.defaultUser = "adam";
-    displayManager.slim.autoLogin = true;
-    displayManager.sessionCommands = ''
-      ${pkgs.numlockx}/bin/numlockx
-      ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
-      ${pkgs.xlibs.xset}/bin/xset r rate 250 50
-      ${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.Xmodmap
-      ${myemacs}/bin/emacs
-    '';
+   programs.bash.enableCompletion = true;
+   programs.mtr.enable = true;
+   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-    desktopManager = {
-      xterm.enable = false;
-      default = "none";
-    };
-    
-  };
+   networking.firewall.allowPing = true;
+   networking.firewall.allowedTCPPorts = [ 22 ];
+   networking.firewall.allowedUDPPorts = [ 22 ];
 
-  fonts.fonts = with pkgs; [
-    source-code-pro
-    font-awesome-ttf
-    powerline-fonts
-  ];
+   sound.enable = true;
+   hardware.pulseaudio.enable = true;
 
-  users.users.adam =
-  { isNormalUser = true;
-    home = "/home/adam";
-    createHome = false;
-    extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" ];
-  };
-  security.sudo.wheelNeedsPassword = false;
+   hardware.opengl = {
+     driSupport = true;
+     driSupport32Bit = true;
+   };
 
-  powerManagement.enable = true;
+   services.tlp.enable = true;
+   services.openssh.enable = true;
+   services.samba.enable = true;
+   services.locate.enable = true;
+   services.printing.enable = true;
+   services.avahi.enable = true;
+   services.avahi.nssmdns = true;
+   services.dnsmasq.enable = true;
+   services.dbus.socketActivated = true;
+
+   virtualisation.libvirtd.enable = true;
+   environment.variables.LIBVIRT_DEFAULT_URI = "qemu:///system";
+
+   users.users.adam =
+     { isNormalUser = true;
+       home = "/home/adam";
+       createHome = false;
+       extraGroups = [ "wheel" "disk" "audio" "video" "systemd-journal"
+         "networkmanager" "libvirtd" ];
+     };
+   security.sudo.wheelNeedsPassword = false;
+
+   services.logind.extraConfig = ''
+     HandleLidSwitchDocked=suspend
+   '';
+
+   security.polkit.enable = true;
+   security.polkit.extraConfig = ''
+     polkit.addRule(function(action, subject) {
+     var YES = polkit.Result.YES;
+     var permission = {
+     "org.freedesktop.udisks2.filesystem-mount": YES,
+     "org.freedesktop.udisks2.filesystem-mount-system": YES,
+     "org.freedesktop.udisks2.eject-media": YES
+     };
+     return permission[action.id];
+     });
+   '';
+
+powerManagement.enable = true;
 
   # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
+    # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "18.03"; # Did you read the comment?
 
-}
-    
-  
+ }
+
+
---
+++ desktop.nix
@@ -0,0 +1,141 @@
+{ config, pkgs, expr, buildVM, ... }:
+
+let
+  myemacs = (pkgs.emacs.override {withGTK3=false; withGTK2=false; withX=true;});
+  iconTheme = pkgs.breeze-icons.out;
+  themeEnv = ''
+    # QT: remove local user overrides (for determinism, causes hard to find bugs)
+    rm -f ~/.config/Trolltech.conf
+
+    # GTK3: remove local user overrides (for determinisim, causes hard to find bugs)
+    rm -f ~/.config/gtk-3.0/settings.ini
+
+    # GTK3: add breeze theme to search path for themes
+    # (currently, we need to use gnome-breeze because the GTK3 version of kde5.breeze is broken)
+    export XDG_DATA_DIRS="${pkgs.gnome-breeze}/share:$XDG_DATA_DIRS"
+
+    # GTK3: add /etc/xdg/gtk-3.0 to search path for settings.ini
+    # We use /etc/xdg/gtk-3.0/settings.ini to set the icon and theme name for GTK 3
+    export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
+
+    # GTK2 theme + icon theme
+    export GTK2_RC_FILES=${pkgs.writeText "iconrc" ''gtk-icon-theme-name="breeze"''}:${pkgs.breeze-gtk}/share/themes/Breeze/gtk-2.0/gtkrc:$GTK2_RC_FILES
+
+    # SVG loader for pixbuf (needed for GTK svg icon themes)
+    export GDK_PIXBUF_MODULE_FILE=$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)
+
+    # QT5: convince it to use our preferred style
+    export QT_STYLE_OVERRIDE=breeze
+  '';
+
+in {
+
+  imports = [];
+
+
+  services.compton = {
+    enable = true;
+    backend = "glx";
+    extraOptions = ''
+      paint-on-overlay = true;
+      vsync = opengl-mswc;
+    '';};
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    libinput.enable = true;
+    libinput.accelSpeed = "0.9";
+    videoDrivers = [ "modesetting" ];
+    useGlamor = true;
+    deviceSection = ''
+      Option "DRI" "3"
+      Option "TearFree" "true"
+      Option "AccelMethod" "glamor"
+    '';
+
+    displayManager.slim.enable = true;
+    displayManager.slim.defaultUser = "adam";
+    displayManager.slim.autoLogin = true;
+    displayManager.sessionCommands = ''
+      ${pkgs.numlockx}/bin/numlockx
+      ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
+      ${pkgs.xlibs.xset}/bin/xset r rate 250 50
+      ${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.Xmodmap
+      ${pkgs.dunst}/bin/dunst &
+      ${myemacs}/bin/emacs
+    '';
+
+    desktopManager = {
+      xterm.enable = false;
+      default = "none";
+    };
+
+  };
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    dejavu_fonts
+    source-code-pro
+    font-awesome-ttf
+    powerline-fonts
+  ];
+
+
+  environment.shells = [
+    "${pkgs.bash}/bin/bash"
+  ];
+
+  environment.sessionVariables = {
+    EDITOR = "emacsclient";
+    VISUAL = "emacsclient";
+    PATH="$HOME/bin:$HOME/.local/bin:$PATH";
+  };
+
+  environment.extraInit = ''
+    ${themeEnv}
+
+    # these are the defaults, but some applications are buggy so we set them
+    # here anyway
+    export XDG_CONFIG_HOME=$HOME/.config
+    export XDG_DATA_HOME=$HOME/.local/share
+    export XDG_CACHE_HOME=$HOME/.cache
+  '';
+
+  # QT5 global theme
+  environment.etc."xdg/Trolltech.conf" = {
+    text = ''
+      [Qt]
+      style=Breeze
+    '';
+    mode = "444";
+  };
+
+  # GTK3 global theme (widget and icon theme)
+  environment.etc."xdg/gtk-3.0/settings.ini" = {
+    text = ''
+      [Settings]
+      gtk-icon-theme-name=breeze
+      gtk-theme-name=Breeze-gtk
+    '';
+    mode = "444";
+  };
+
+  environment.systemPackages = with pkgs; [
+    # Qt theme
+    breeze-qt5
+
+    # Icons (Main)
+    iconTheme
+
+    # Icons (Fallback)
+    gnome3.adwaita-icon-theme
+    hicolor_icon_theme
+  ];
+
+  # Make applications find files in <prefix>/share
+environment.pathsToLink = [ "/share" ];
+
+}
