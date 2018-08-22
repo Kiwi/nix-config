e8d5383bf7974540dbe75b366b1ae25e871b2fcd
Adam Schaefers <sch@efers.org>
2018-08-22 16:51:32 -0700

    commentary

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,6 @@
- { ... }: { imports = [
-   /etc/nixos/hardware-configuration.nix
-   /nixcfg/init.nix
+ { ... }:
+# this can be a symlink in /etc/nixos/ or the actual file.
+{ imports = [
+  /etc/nixos/hardware-configuration.nix
+  /nixcfg/init.nix
  ]; }
