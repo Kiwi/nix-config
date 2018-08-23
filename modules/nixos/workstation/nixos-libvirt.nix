32349f962088e88dea5251891d2d457d51f0e1f4
Adam Schaefers <sch@efers.org>
2018-08-23 14:47:54 -0700

    whitespace

--- modules/nixos/workstation/nixos-libvirt.nix
+++ modules/nixos/workstation/nixos-libvirt.nix
@@ -4,8 +4,8 @@ with lib;
     imports = [  ];
 
     options.mine.libvirtd.enable = mkEnableOption "Libvirtd Profile";
-
     config = mkIf config.mine.libvirtd.enable {
+
       services.dnsmasq.enable = true;
       virtualisation.libvirtd.enable = true;
       environment.variables.LIBVIRT_DEFAULT_URI = "qemu:///system";
