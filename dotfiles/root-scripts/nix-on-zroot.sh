0691d089b0d3e1dc6bc0f11d77237319174548d2
Adam Schaefers <sch@efers.org>
2018-08-28 17:38:51 -0700

    wip

--- dotfiles/root-scripts/nix-on-zroot.sh
+++ dotfiles/root-scripts/nix-on-zroot.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# A simple nix-on-zroot installer.
+# Usage : nix-on-zroot -g <git-url> -h <host.nix>
 
 # Copyright 2018 sch@efers.org
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-# Set variables below and then feed the script a .nix file from hosts/
-# usage: ./nix-on-zroot.sh HOST.nix
-
-#################
-# set variables #
-#################
 
 # TODO GET from $1
 # Your personal nix-config repo to be bootstrapped
