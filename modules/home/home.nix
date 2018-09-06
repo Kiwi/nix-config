f3275e1136749e588e81c7a569c43e31ce6b46a3
Adam Schaefers <sch@efers.org>
2018-09-06 15:12:01 -0700

    remove home-manager

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,85 +1,72 @@
 { config, pkgs, lib, ... }:
-# all $HOME and user configurations
+
 with lib;
 let
-  adamDotfiles = "/nix-config/dotfiles";
 
-  cloneRepos = pkgs.writeScriptBin "mynixos-cloneRepos" ''
-    #!${pkgs.stdenv.shell}
-    # clone all of my favorite repos
-    mkdir ~/repos/
-    cd ~/repos/
-    git clone git@github.com:cooslug/cooslug.github.io.git
-    git clone git@github.com:a-schaefers/a-schaefers.github.io.git
-    git clone git@github.com:a-schaefers/grubbe-mkconfig.git
-    git clone git@github.com:TemptorSent/Funtools.git
-    git clone git@github.com:apoptosis/episteme.git
-    git clone git@github.com:dustinlacewell/emacs-nougat.git
-    git clone git@github.com:bbatsov/prelude.git
-    git clone git@github.com:a-schaefers/themelios.git
-  '';
+adamSymlinks = pkgs.writeScriptBin "mynixos-symlinks" ''
+#!${pkgs.stdenv.shell}
+mkdir ~/.emacs.d
+mkdir -p ~/.config/{mpv,mimi}
+ln -sf /nix-config/dotfiles/bin ~/bin
+ln -sf /nix-config/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf /nix-config/dotfiles/.emacs.d/lisp.d ~/.emacs.d/lisp.d
+ln -sf /nix-config/dotfiles/.Xmodmap ~/.Xmodmap
+ln -sf /nix-config/dotfiles/.gitconfig ~/.gitconfig
+ln -sf /nix-config/dotfiles/.bash_profile ~/.bash_profile
+ln -sf /nix-config/dotfiles/.bashrc ~/.bashrc
+ln -sf /nix-config/dotfiles/.inputrc ~/.inputrc
+ln -sf /nix-config/dotfiles/.config/mimi/mime.conf ~/.config/mimi/mime.conf
+ln -sf /nix-config/dotfiles/.mailcap ~/.mailcap
+ln -sf /nix-config/dotfiles/.config/mpv/mpv.conf ~/.config/mpv/mpv.conf
+'';
 
-  cleanHome = pkgs.writeScriptBin "mynixos-cleanHome" ''
-    #!${pkgs.stdenv.shell}
-    # yes i know this is rediculous, but what the hay.
-    cd ~/
-    find -name "*" | egrep -v \
-    "bash_history|ssh|gnupg|gpg|mozilla|qBittorrent|emacs|slime|repos|Documents|Downloads" \
-    | xargs rm -f
-    find . -type d -empty -delete
-    systemctl restart home-manager-adam || exit
-    pkill emacs
-  '';
+cloneRepos = pkgs.writeScriptBin "mynixos-cloneRepos" ''
+#!${pkgs.stdenv.shell}
+# clone all of my favorite repos
+mkdir ~/repos/
+cd ~/repos/
+git clone git@github.com:cooslug/cooslug.github.io.git
+git clone git@github.com:a-schaefers/themelios.git
+git clone git@github.com:a-schaefers/grubbe-mkconfig.git
+git clone git@github.com:apoptosis/episteme.git
+git clone git@github.com:a-schaefers/a-schaefers.github.io.git
+git clone git@github.com:TemptorSent/Funtools.git
+git clone git@github.com:bbatsov/prelude.git
+'';
+
+cleanHome = pkgs.writeScriptBin "mynixos-cleanHome" ''
+#!${pkgs.stdenv.shell}
+# Yes, I know this is rediculous, but what the hay guys :)
+cd ~/
+find -name "*" | egrep -v \
+"bash_history|ssh|gnupg|gpg|mozilla|qBittorrent|emacs|slime|repos|Documents|Downloads" \
+| xargs rm -f
+find . -type d -empty -delete
+pkill emacs
+'';
 in
-  {
-    imports = [
-      # make home-manager available https://nixos.wiki/wiki/Home_Manager
-      "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
-    ];
+{
+imports = [];
 
-    options.mine.home.enable = mkEnableOption "Home Profile";
-    config = mkIf config.mine.home.enable {
+options.mine.home.enable = mkEnableOption "Home Profile";
+config = mkIf config.mine.home.enable {
 
-      # system-wide developer environment packages
-      environment.systemPackages = with pkgs; [
-        cloneRepos
-        cleanHome
-        gitAndTools.gitFull gitAndTools.gitflow
-        tmux
-      ];
+# system-wide developer environment packages
+environment.systemPackages = with pkgs; [
+cloneRepos
+cleanHome
+adamSymlinks
+];
 
-      # setup users
-      users.users.adam =
-        { isNormalUser = true;
-          home = "/home/adam";
-          createHome = true;
-          extraGroups = [ "wheel" "disk" "audio" "video" "systemd-journal"
-            "networkmanager" "libvirtd" ];
-        };
-      security.sudo.wheelNeedsPassword = false;
+# setup users
+users.users.adam =
+{ isNormalUser = true;
+home = "/home/adam";
+createHome = true;
+extraGroups = [ "wheel" "disk" "audio" "video" "systemd-journal"
+"networkmanager" "libvirtd" ];
+};
+security.sudo.wheelNeedsPassword = false;
 
-      # home-manager for "adam" section
-      home-manager.users.adam = {
-        # create symlinks to my dotfiles (recreated every time the home-manager-adam service restarts)
-        home.file.".emacs.d/init.el".source = "${adamDotfiles}/.emacs.d/init.el";
-        home.file.".emacs.d/lisp.d".source = "${adamDotfiles}/.emacs.d/lisp.d";
-        home.file.".Xmodmap".source = "${adamDotfiles}/.Xmodmap";
-        home.file.".gitconfig".source = "${adamDotfiles}/.gitconfig";
-        home.file.".bash_profile".source = "${adamDotfiles}/.bash_profile";
-        home.file.".bashrc".source = "${adamDotfiles}/.bashrc";
-        home.file.".inputrc".source = "${adamDotfiles}/.inputrc";
-        home.file."/bin".source = "${adamDotfiles}/bin";
-        home.file."/.config/mimi/mime.conf".source = "${adamDotfiles}/.config/mimi/mime.conf";
-        home.file.".mailcap".source = "${adamDotfiles}/.mailcap";
-        home.file."/.config/mpv/mpv.conf".source = "${adamDotfiles}/.config/mpv/mpv.conf";
-      };
-
-      home-manager.users.root = {
-        home.file.".bash_profile".source = "${adamDotfiles}/.bash_profile";
-        home.file.".bashrc".source = "${adamDotfiles}/.bashrc";
-        home.file.".inputrc".source = "${adamDotfiles}/.inputrc";
-        home.file."/bin".source = "${adamDotfiles}/root-scripts";
-      };
-
-    };
-  }
+};
+}
--- modules/nixos/workstation/nixos-workstation.nix
+++ modules/nixos/workstation/nixos-workstation.nix
@@ -1,98 +1,100 @@
 { config, pkgs, lib, ... }:
 
 with lib;
-  let
-    myEmacs = (pkgs.emacs.override {withGTK3=false; withGTK2=false; withX=true;});
-  in
+let
+myEmacs = (pkgs.emacs.override {withGTK3=false; withGTK2=false; withX=true;});
+in
 {
-  options.mine.workstation.enable = mkEnableOption "Workstation Profile";
+options.mine.workstation.enable = mkEnableOption "Workstation Profile";
 
-  config = mkIf config.mine.workstation.enable {
+config = mkIf config.mine.workstation.enable {
 
-    # misc services I don't really use
+# misc services I don't really use
 
-    # services.samba.enable = true;
-    # services.locate.enable = true;
-    # services.printing.enable = true;
-    # services.avahi.enable = true;
-    # services.avahi.nssmdns = true;
+# services.samba.enable = true;
+# services.locate.enable = true;
+# services.printing.enable = true;
+# services.avahi.enable = true;
+# services.avahi.nssmdns = true;
 
-    services.acpid.enable = true;
-    services.smartd.enable = true;
+services.acpid.enable = true;
+services.smartd.enable = true;
 
-    # pulseaudio
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+# pulseaudio
+sound.enable = true;
+hardware.pulseaudio.enable = true;
 
-    # mesa
-    hardware.opengl = {
-      driSupport = true;
-      driSupport32Bit = true;
-    };
+# mesa
+hardware.opengl = {
+driSupport = true;
+driSupport32Bit = true;
+};
 
-    # Xorg, Slim, Emacs
-    services.xserver = {
-      enable = true;
-      layout = "us";
-      useGlamor = true;
-      displayManager.slim.enable = true;
-      displayManager.slim.autoLogin = false;
-      displayManager.slim.defaultUser = "adam";
-      displayManager.sessionCommands = ''
-        ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
-        ${pkgs.xlibs.xset}/bin/xset r rate 250 50
-        ${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.Xmodmap
-        ${pkgs.numlockx}/bin/numlockx
-        ${pkgs.dunst}/bin/dunst &
-      '';
-      desktopManager = {
-        xterm.enable = false;
-        default = "emacs";
-        session = [ {
-          manage = "desktop";
-          name = "emacs";
-          start = ''
-            ${myEmacs}/bin/emacs &
-            waitPID=$!
-          '';}];};};
+# Xorg, Slim, Emacs
+services.xserver = {
+enable = true;
+layout = "us";
+useGlamor = true;
+displayManager.slim.enable = true;
+displayManager.slim.autoLogin = false;
+displayManager.slim.defaultUser = "adam";
+displayManager.sessionCommands = ''
+${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
+${pkgs.xlibs.xset}/bin/xset r rate 250 50
+${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.Xmodmap
+${pkgs.numlockx}/bin/numlockx
+${pkgs.dunst}/bin/dunst &
+'';
+desktopManager = {
+xterm.enable = false;
+default = "emacs";
+session = [ {
+manage = "desktop";
+name = "emacs";
+start = ''
+${myEmacs}/bin/emacs &
+waitPID=$!
+'';}];};};
 
-    environment.sessionVariables = {
-      EDITOR = "emacsclient";
-      VISUAL = "emacsclient";
-      _JAVA_AWT_WM_NONREPARENTING = "1";
-    };
+environment.sessionVariables = {
+EDITOR = "emacsclient";
+VISUAL = "emacsclient";
+_JAVA_AWT_WM_NONREPARENTING = "1";
+};
 
-    # some basic xft fonts
-    fonts.fonts = with pkgs; [
-      dejavu_fonts
-      source-code-pro ];
+# some basic xft fonts
+fonts.fonts = with pkgs; [
+dejavu_fonts
+source-code-pro ];
 
-    environment.systemPackages = with pkgs; [
-      # Custom Emacs and some Emacs deps used by my config
-      myEmacs
-      shellcheck
-      poppler_utils poppler_gi libpng12 zlib
-      (python36.withPackages(ps: with ps; [ certifi ]))
-      gnutls gnupg gnupg1compat
+environment.systemPackages = with pkgs; [
+# Custom Emacs and some Emacs deps used by my config
+myEmacs
+gitAndTools.gitFull gitAndTools.gitflow
+tmux
+shellcheck
+poppler_utils poppler_gi libpng12 zlib
+(python36.withPackages(ps: with ps; [ certifi ]))
+gnutls gnupg gnupg1compat
 
-      # desktop support applications
-      glxinfo
-      wmctrl
-      scrot
-      xorg.xmodmap xorg.xev xorg.xrdb xorg.xset xorg.xsetroot
-      numlockx
-      xclip xsel
-      libnotify dunst
+# desktop support applications
+glxinfo
+wmctrl
+scrot
+xorg.xmodmap xorg.xev xorg.xrdb xorg.xset xorg.xsetroot
+numlockx
+xclip xsel
+libnotify dunst
 
-      # desktop apps
-      pandoc
-      firefox qbittorrent mpv pavucontrol
-      gimp kdenlive darktable krita inkscape
+# desktop apps
+pandoc
+firefox qbittorrent mpv pavucontrol
+gimp kdenlive darktable krita inkscape
 
-      # security
-      openvpn
-    ];
+# security
+openvpn
+];
 
-    programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-    };
+programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+};
 }
