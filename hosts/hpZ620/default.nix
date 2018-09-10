fcb805a24e1f7437da24efc5acba5894be3ffe11
Adam Schaefers <sch@efers.org>
2018-09-09 23:12:18 -0700

    add passthrough

--- hosts/hpZ620/default.nix
+++ hosts/hpZ620/default.nix
@@ -6,7 +6,8 @@ imports = [ ../../modules ];
 networking.hostName = "hpZ620";
 
 # hardware specific
-boot.kernelModules = [ "microcode" "coretemp" ];
+boot.kernelModules = [ "microcode" "coretemp" "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
+boot.extraModprobeConfig ="options vfio-pci ids=10de:0dd8,10de:0be9";
 boot.kernelParams = [ "intel_iommu=on iommu=pt" "amdgpu.audio=0" "amdgpu.dc=1" ];
 boot.initrd.kernelModules = [ "amdgpu" ];
 
