c47a56b73f1512ebb8048c5c7abafd5ee4e0539c
Adam Schaefers <sch@efers.org>
2018-09-12 14:54:06 -0700

    hardware vaapi and vdpau decoding for intel gpus

--- hosts/latitudeE6430/default.nix
+++ hosts/latitudeE6430/default.nix
@@ -5,17 +5,17 @@ imports = [ ../../modules ];
 
 networking.hostName = "latitudeE6430";
 
+# my modules
+modules.general.enable = true;
+modules.desktop.enable = true;
+modules.libvirtd.enable = true;
+
 # hardware specific
 nix.buildCores = 0;
 nix.maxJobs = 4;
 boot.kernelModules = [ "microcode" "coretemp" ];
 boot.kernelParams = [ "i915.enable_fbc=1" ];
 boot.initrd.kernelModules = [ "i915" ];
-
-# my modules
-modules.general.enable = true;
-modules.desktop.enable = true;
-modules.libvirtd.enable = true;
 
 # boost laptop power savings
 services.tlp.enable = true;
@@ -24,7 +24,7 @@ services.tlp.enable = true;
 services.xserver.libinput.enable = true;
 services.xserver.libinput.accelSpeed = "0.9";
 
-# video
+# intel laptop video
 services.xserver.videoDrivers = [ "modesetting" ];
 services.xserver.deviceSection = ''
 Option "DRI" "3"
@@ -37,9 +37,17 @@ enable = true;
 backend = "glx";
 };
 
-programs.light.enable = true;
+hardware.opengl.extraPackages = with pkgs;
+[ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux;
+[ vaapiIntel libvdpau-va-gl vaapiVdpau ];
 
-networking.enableIPv6 = false;
+environment.sessionVariables = {
+LIBVA_DRIVER_NAME = "i965";
+VDPAU_DRIVER = "va_gl";
+};
+
+programs.light.enable = true;
 
 # This value determines the NixOS release with which your system is to be
 # compatible, in order to avoid breaking some software such as database
