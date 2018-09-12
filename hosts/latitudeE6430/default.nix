86dba99999fa342ab51afab991e9c29c9fba7cdd
Adam Schaefers <sch@efers.org>
2018-09-12 12:41:31 -0700

    no ipv6 for laptop

--- hosts/latitudeE6430/default.nix
+++ hosts/latitudeE6430/default.nix
@@ -39,6 +39,8 @@ backend = "glx";
 
 programs.light.enable = true;
 
+networking.enableIPv6 = false;
+
 # This value determines the NixOS release with which your system is to be
 # compatible, in order to avoid breaking some software such as database
 # servers. You should change this only after NixOS release notes say you
