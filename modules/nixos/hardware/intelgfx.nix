e32b14b91e7dcaf196ec48104465bd6d171f716b
Adam Schaefers <sch@efers.org>
2018-09-14 18:27:30 -0700

    intelgfx update

--- modules/nixos/hardware/intelgfx.nix
+++ modules/nixos/hardware/intelgfx.nix
@@ -4,6 +4,9 @@ with lib;
 options.modules.hardware.intelgfx.enable = mkEnableOption "modules.hardware.intelgfx";
 config = mkIf config.modules.hardware.intelgfx.enable {
 
+boot.kernelParams = [ "i915.enable_fbc=1" ];
+boot.initrd.kernelModules = [ "i915" ];
+
 services.xserver.videoDrivers = [ "modesetting" ];
 services.xserver.deviceSection = ''
 Option "DRI" "3"
--- modules/nixos/hardware/platforms/latitudeE6430.nix
+++ modules/nixos/hardware/platforms/latitudeE6430.nix
@@ -5,9 +5,6 @@ with lib;
 options.modules.hardware.platform.latitudeE6430.enable = mkEnableOption "hardware.platform.latitudeE6430";
 config = mkIf config.modules.hardware.platform.latitudeE6430.enable {
 
-boot.kernelParams = [ "i915.enable_fbc=1" ];
-boot.initrd.kernelModules = [ "i915" ];
-
 modules.hardware.enable = true;
 modules.hardware.power.laptops.enable = true;
 modules.hardware.intelgfx.enable = true;
