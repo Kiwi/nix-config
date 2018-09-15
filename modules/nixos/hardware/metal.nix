c5e673cc4ae2e489d2ac20c693ab98bbebe354e7
Adam Schaefers <sch@efers.org>
2018-09-15 02:36:11 -0700

    remove sketchy opt

--- modules/nixos/hardware/metal.nix
+++ modules/nixos/hardware/metal.nix
@@ -5,8 +5,6 @@ with lib;
 options.modules.hardware.metal.enable = mkEnableOption "modules.hardware";
 config = mkIf config.modules.hardware.metal.enable {
 
-nix.buildCores = 0;
-
 boot.kernelModules = [ "coretemp" ];
 
 services.acpid.enable = true;
