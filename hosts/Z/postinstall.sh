6c1fa2bad8f700dc881dee487675991ef2495bf0
Adam Schaefers <sch@efers.org>
2018-09-16 23:22:17 -0700

    add heredoc

--- hosts/Z/postinstall.sh
+++ hosts/Z/postinstall.sh
@@ -1,3 +1,4 @@
+# get my keys off my usb stick
 mkdir /tmp/Private
 mount /dev/disk/by-id/usb-PNY_USB_2.0_FD_A0708H32YD34001168-0:0 /tmp/Private
 cp -rp /tmp/Private/Private /mnt/"${nix_repo_name}"/dotfiles
@@ -5,3 +6,9 @@ umount /tmp/Private
 
 # make my symlinks
 /mnt/nix-config/dotfiles/bin/ghettolinker.sh
+
+# own my files
+cat << EOF | nixos-enter
+chown -R adam:users /nix-config
+chown -R adam:users /home/adam
+EOF
--- hosts/adamant/postinstall.sh
+++ hosts/adamant/postinstall.sh
@@ -1,4 +1,4 @@
-# move my keys from my usb drive to my new nixos installation
+# get my keys off my usb stick
 mkdir /tmp/Private
 mount /dev/disk/by-id/usb-PNY_USB_2.0_FD_A0708H32YD34001168-0:0 /tmp/Private
 cp -rp /tmp/Private/Private /mnt/"${nix_repo_name}"/dotfiles
@@ -6,3 +6,9 @@ umount /tmp/Private
 
 # make my symlinks
 /mnt/nix-config/dotfiles/bin/ghettolinker.sh
+
+# own my files
+cat << EOF | nixos-enter
+chown -R adam:users /nix-config
+chown -R adam:users /home/adam
+EOF
