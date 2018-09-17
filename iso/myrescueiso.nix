5c4cf95af7d94b09d58acbbbb96e9b069fa463b5
Adam Schaefers <sch@efers.org>
2018-09-17 10:51:53 -0700

    reformat

--- iso/myrescueiso.nix
+++ iso/myrescueiso.nix
@@ -1,23 +1,21 @@
 # nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=myrescueiso.nix
 {config, pkgs, ...}:
 let
-  themelios = pkgs.writeScriptBin "themelios" ''
-    bash <(curl https://raw.githubusercontent.com/a-schaefers/themelios/master/themelios) $@
-  '';
+themelios = pkgs.writeScriptBin "themelios" ''
+bash <(curl https://raw.githubusercontent.com/a-schaefers/themelios/master/themelios) $@
+'';
 in {
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  ];
+imports = [
+<nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+<nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+];
 
-  networking = {
-  networkmanager.enable = true;
-    wireless.enable = false;
-    nameservers = [ "8.8.8.8" "8.8.4.4" ];
-    firewall.allowPing = true;
-    firewall.allowedTCPPorts = [ 22 ];
-    firewall.allowedUDPPorts = [ 22 ];
-  };
-  boot.supportedFilesystems = [ "zfs" ];
-  environment.systemPackages = with pkgs; [ git themelios ];
+networking = {
+networkmanager.enable = true;
+wireless.enable = false;
+nameservers = [ "8.8.8.8" "8.8.4.4" ];
+};
+
+boot.supportedFilesystems = [ "zfs" ];
+environment.systemPackages = with pkgs; [ git themelios ];
 }
