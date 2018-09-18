6553811333343f2977d4316072a4f01f74b2061b
Adam Schaefers <sch@efers.org>
2018-09-17 18:35:28 -0700

    --no-root-passwd

--- hosts/Z/configuration.sh
+++ hosts/Z/configuration.sh
@@ -45,7 +45,7 @@ nix_top_level_configuration="hosts/Z"
 nix_repo_name="nix-config"
 
 # Optionally inserted as "nixos-install --root /mnt $nix_install_opts"
-nix_install_opts="--max-jobs 24 --cores 0"
+nix_install_opts="--max-jobs 24 --cores 0 --no-root-passwd"
 
 # Creates /etc/nixos/zfs-configuration.nix with sensible settings.
 nix_zfs_configuration_enabled="true"
